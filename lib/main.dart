35f5d1aae1472e30b7c869b0e079f33d5a37abcf
NakanoNamiki <namiki.nakano.vertex@gmail.com>
2021-06-20 04:29:33 +0900

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'hello_flutter',
       theme: ThemeData(primaryColor: Colors.green),
       home: RandomWords(),
